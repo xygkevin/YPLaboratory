db1b29a07acbdd3de627c234ce3a0ab78cd6f7d9
chenghengsheng <chenghs@shinemo.com>
2023-05-21 22:26:28 +0800

    feature: 增加侧滑退出功能
    Signed-off-by: chenghengsheng <chenghs@shinemo.com>

--- YPLaboratory/YPLaboratory/Tools/Base/YPNavigationViewController.m
+++ YPLaboratory/YPLaboratory/Tools/Base/YPNavigationViewController.m
@@ -7,7 +7,7 @@
 
 #import "YPNavigationViewController.h"
 
-@interface YPNavigationViewController ()
+@interface YPNavigationViewController () <UIGestureRecognizerDelegate>
 
 @end
 
@@ -16,6 +16,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+    [self.navigationBar setShadowImage:[UIImage new]];
+    self.navigationBar.titleTextAttributes = @{NSFontAttributeName:[UIFont boldSystemFontOfSize:18.f]};
+    if ([self respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
+        self.interactivePopGestureRecognizer.delegate = self;
+    }
+}
+
+#pragma mark - UIGestureRecognizerDelegate
+//当手势开始滑动作用：拦截手势触发
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    [[YPShakeManager shareInstance] tapShare];
+    //子控制器个数只剩下一个(这一个就是根控制器),手势不可用
+    NSArray *vcs = self.childViewControllers;
+    if (vcs.count > 1) {
+        YPBaseViewController *vc = vcs.lastObject;
+        if ([vc isKindOfClass:[YPBaseViewController class]]) {
+            return [vc allowSideslip];
+        } else {
+            return YES;
+        }
+    }
+    BOOL open = vcs.count != 1;
+    return open;
 }
 
 @end
